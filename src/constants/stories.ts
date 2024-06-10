d1d36cc19e1873ee0df4471fd495223228cba2b8
Maxim Rudin <>
2024-06-10 18:58:50 +0500

    ed photo

--- src/constants/stories.ts
+++ src/constants/stories.ts
@@ -5,33 +5,27 @@ export interface BeforeAfterStory {
 
 export const stories: BeforeAfterStory[] = [
   {
-    image: '/stories/1.jpg',
-    description:
-      'История 1: Описание изменений после.',
+    image: '/stories/1.webp',
+    description: 'История 1: Описание изменений после.',
   },
   {
-    image: '/stories/2.jpg',
-    description:
-      'История 2: Описание изменений после .',
+    image: '/stories/2.webp',
+    description: 'История 2: Описание изменений после .',
   },
   {
-    image: '/stories/3.jpg',
-    description:
-      'История 3: Описание изменений после.',
+    image: '/stories/3.webp',
+    description: 'История 3: Описание изменений после.',
   },
   {
-    image: '/stories/4.jpg',
-    description:
-      'История 4: Описание изменений после.',
+    image: '/stories/4.webp',
+    description: 'История 4: Описание изменений после.',
   },
   {
-    image: '/stories/5.jpg',
-    description:
-      'История 5: Описание изменений после.',
+    image: '/stories/5.webp',
+    description: 'История 5: Описание изменений после.',
   },
   {
-    image: '/stories/6.jpg',
-    description:
-      'История 6: Описание изменений после .',
+    image: '/stories/6.webp',
+    description: 'История 6: Описание изменений после .',
   },
 ];
