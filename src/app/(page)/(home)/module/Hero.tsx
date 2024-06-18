8b6018a4c106153f6c004acff1ab2095ec0e6b51
Maxim Rudin <>
2024-06-19 00:40:58 +0500

    add

--- src/app/(page)/(home)/module/Hero.tsx
+++ src/app/(page)/(home)/module/Hero.tsx
@@ -1,5 +1,6 @@
-import { MAIN_PHONE_NUMBER } from '@/constants/phone';
+import { MAIN_PHONE_NUMBER, SECOND_PHONE_NUMBER } from '@/constants/phone';
 import Link from 'next/link';
+import { FaTelegram, FaWhatsapp } from 'react-icons/fa';
 import style from '../Home.module.scss';
 
 interface HeroProps {
@@ -17,15 +18,30 @@ export function Hero({ title, subtitle, description, buttonText }: HeroProps) {
         <h2 className={style.hero__subtitle}>{subtitle}</h2>
         <p className={style.hero__description}>{description}</p>
         <div className={style.hero__list__button}>
-            <Link href="/#contactUs" className={style.hero__button}>
-              {buttonText}
-            </Link>
-            <Link
-              href={`tel:${MAIN_PHONE_NUMBER}`}
-              className={style.hero__button_second}
-            >
-              Заказать звонок
-            </Link>
+          <Link href={`https://wa.me/${SECOND_PHONE_NUMBER.replace(
+            '+',
+            ''
+          )}`} className={style.hero__button} rel="noopener noreferrer" target='_blank'>
+            <FaWhatsapp />
+
+            Написать в WhatsApp
+          </Link>
+          <Link
+            href={`https://t.me/Max_Rudin`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={style.hero__button_second}
+          >
+            <FaTelegram /> Написать в Telegram
+          </Link>
+        </div>
+        <div >
+          <Link
+            href={`tel:${MAIN_PHONE_NUMBER}`}
+            className={style.hero__button_second}
+          >
+            Заказать звонок
+          </Link>
         </div>
       </div>
     </section >
