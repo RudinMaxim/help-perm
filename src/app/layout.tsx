f10f73ac968a0c5344cf8c70ef035a094da008bd
Maxim Rudin <>
2024-06-19 23:41:43 +0500

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Footer, Header } from '@/components';
 import { getMetadata } from '@/utils/getMetadata';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const poppins = Poppins({ subsets: ['latin'], weight: '400' });
@@ -40,6 +41,23 @@ export default function RootLayout({
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/97610015" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter --> */}
+        {/* <!-- Yandex.Metrika counter --> */}
+        <Script id="yandex-metrika-counter" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(97610015, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+        
+   })
+   <noscript><div><img src="https://mc.yandex.ru/watch/97610015" style="position:absolute; left:-9999px;" alt="" /></div></noscript>`}
+        </Script>
 
       </body>
     </html>
