c2f29c542ac1ecbc5deb621ab1b8756db02ecebd
Maxim Rudin <>
2024-06-22 14:14:01 +0500

    add

--- src/components/MotivationalBanner/MotivationalBanner.tsx
+++ src/components/MotivationalBanner/MotivationalBanner.tsx
@@ -6,7 +6,7 @@ export function MotivationalBanner() {
   return (
     <article className={styles.banner}>
       <div className={styles.content}>
-        <h2 className={styles.title}>Не откладывай помощь на потом – свяжись с нами прямо сейчас!</h2>
+        <h2 className={styles.title}>Не откладывай помощь&nbsp;на потом – свяжись с нами прямо сейчас!</h2>
         <p className={styles.description}>
           Жизненные трудности могут сделать нас уязвимыми и заставить
           чувствовать себя одинокими. Но помните, что вы не одиноки в своих
