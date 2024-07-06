9b27f47b3b113ca6b12c270761e6fa852594b056
Maxim Rudin <>
2024-07-07 00:26:29 +0500

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { Footer, Header } from '@/components';
 import { getMetadata } from '@/utils/getMetadata';
 import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
-import './globals.css';
+import { Inter } from 'next/font/google';
+import './globals.scss';
 
-const poppins = Poppins({ subsets: ['latin'], weight: '400' });
+const poppins = Inter({ subsets: ['latin'], weight: '400' });
 
 export const metadata: Metadata = getMetadata({
   title: 'Помощь рядом',
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../app/globals.css';
+import '../../app/globals.scss';
 
 export function Container({ children }: { children: React.ReactNode }) {
 	return <div className='container'>{children}</div>;
