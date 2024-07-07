69284bcd65445b82a0bba99a92c46588d1e04478
MaxRudin <rudin.maksimka@bk.ru>
2024-07-07 17:42:16 +0500

    add

--- src/hook/useWhatsAppMessage.ts
+++ src/hook/useWhatsAppMessage.ts
@@ -1,3 +1,4 @@
+import { MAIN_PHONE_NUMBER } from '@/constants/phone';
 import { useState } from 'react';
 
 interface MessageData {
@@ -13,36 +14,38 @@ export const useWhatsAppMessage = (phoneNumber: string) => {
     message: '',
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
-    
+
     if (name === 'phone') {
       // Удаляем все нецифровые символы
       let cleanedValue = value.replace(/\D/g, '');
-      
+
       // Убираем первую цифру, если она 7 или 8
       if (cleanedValue.startsWith('7') || cleanedValue.startsWith('8')) {
         cleanedValue = cleanedValue.slice(1);
       }
-      
+
       // Ограничиваем длину до 10 цифр
       cleanedValue = cleanedValue.slice(0, 10);
-      
+
       // Если первая цифра не 9, заменяем ее на 9
       if (cleanedValue.length > 0 && cleanedValue[0] !== '9') {
         cleanedValue = '9' + cleanedValue.slice(1);
       }
 
-      setMessageData(prev => ({ ...prev, [name]: cleanedValue }));
+      setMessageData((prev) => ({ ...prev, [name]: cleanedValue }));
     } else {
-      setMessageData(prev => ({ ...prev, [name]: value }));
+      setMessageData((prev) => ({ ...prev, [name]: value }));
     }
   };
 
   const sendMessage = () => {
     const { name, phone, message } = messageData;
     const fullPhone = `7${phone}`;
-    const whatsappMessage = `https://wa.me/${fullPhone}?text=Имя: ${name}%0AТелефон: +7${phone}%0AСообщение: ${message}`;
+    const whatsappMessage = `https://wa.me/${MAIN_PHONE_NUMBER}?text=Имя: ${name}%0AТелефон: +7${phone}%0AСообщение: ${message}`;
     window.open(whatsappMessage, '_blank');
   };
 
